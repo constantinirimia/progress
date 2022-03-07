69cb1c93cdc1cffde8026739cb5bf4e90cf798a5
Constantin <cirimia100@gmail.com>
2022-03-07 15:49:38 -0600

    S

---
+++ build/precache-manifest.54a323a92230d1509c5e07e13ee5725e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25da3a1cf432af2f2b8bb1ef5b3f8f6e",
+    "url": "/progress/index.html"
+  },
+  {
+    "revision": "5f5578513bcfdbba3aa1",
+    "url": "/progress/static/css/2.a07d8c99.chunk.css"
+  },
+  {
+    "revision": "85dc59363b1c5b2fbe6f",
+    "url": "/progress/static/css/main.03524c76.chunk.css"
+  },
+  {
+    "revision": "5f5578513bcfdbba3aa1",
+    "url": "/progress/static/js/2.7eb24393.chunk.js"
+  },
+  {
+    "revision": "5467e851392549ae3564d75adbc67604",
+    "url": "/progress/static/js/2.7eb24393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85dc59363b1c5b2fbe6f",
+    "url": "/progress/static/js/main.5447a4a5.chunk.js"
+  },
+  {
+    "revision": "9915c023f8018a38fe89",
+    "url": "/progress/static/js/runtime-main.80eb52b3.js"
+  }
+]);
